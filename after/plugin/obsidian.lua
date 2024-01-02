3358a0163cc8fbfae553c5ec435e7136f4170589
eyesofbucket <andrew.thompson@geeksandnerds.com>
2024-01-02 01:24:44 -0600

    Fixed undodir, obsidian, mouse

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -1,13 +1,3 @@
 require("obsidian").setup({
-    workspaces = {
-        {
-            name = "EyesofBucket",
-            path = "~/Obsidian/EyesofBucket",
-        },
-        {
-            name = "Dungeons and Dragons",
-            path = "~/Obsidian/Dungeons and Dragons",
-        }
-    },
     disable_frontmatter = true
 })
--- lua/bvkt/set.lua
+++ lua/bvkt/set.lua
@@ -10,8 +10,14 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+if (vim.loop.os_uname().sysname == "Windows_NT")
+then
+    vim.opt.undodir = os.getenv("USERPROFILE") .. "/.vim/undodir"
+else
+    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+end
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -25,3 +31,5 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.mouse = ""
