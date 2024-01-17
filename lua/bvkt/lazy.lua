870dd1a39f5dc69ead92c8018401d66dc81af12f
eyesofbucket <eyesofbucket@example.com>
2024-01-17 01:54:03 -0600

    Improved telescope performance

--- lua/bvkt/lazy.lua
+++ lua/bvkt/lazy.lua
@@ -180,6 +180,12 @@ require("lazy").setup({
     },
     {
         "nvim-telescope/telescope.nvim",
+        dependencies = {
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+            },
+        },
         version = "0.1.3",
         keys = {
             { "<leader>pf", function() require('telescope.builtin').find_files() end, desc = "Telescope: Files" },
