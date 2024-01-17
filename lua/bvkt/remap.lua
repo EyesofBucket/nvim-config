941e9114dcf726bd7f093770d8fbe87affe48f2d
eyesofbucket <eyesofbucket@example.com>
2024-01-16 18:10:28 -0600

    Added title case keybind

--- lua/bvkt/remap.lua
+++ lua/bvkt/remap.lua
@@ -5,11 +5,14 @@ vim.g.mapleader = " "
 wk.register({
 p = {
     name = "Panes",
-    v = { vim.cmd.Ex, "Netrw" },
+    v = "Netrw",
   },
 }, { prefix = "<leader>" })
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("v", "<leader>gt", ":s/\\v<%((.)%(\\w{3,})@=|(i>))(\\w*)/\\u\\1\\L\\2\\3/g<CR>")
