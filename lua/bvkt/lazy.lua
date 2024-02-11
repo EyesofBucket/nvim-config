d1f258853cf3f79c84ed19fd57549f50b95acc0e
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2024-02-11 12:53:46 -0600

    Fixed LSP

--- lua/bvkt/lazy.lua
+++ lua/bvkt/lazy.lua
@@ -111,50 +111,75 @@ require("lazy").setup({
             extensions = {}
         },
     },
-    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-    "neovim/nvim-lspconfig",
-    "williamboman/mason-lspconfig.nvim",
     {
-        "williamboman/mason.nvim",
-        cmd = "Mason",
-        build = ":MasonUpdate",
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+        },
         keys = {
             { "<leader>pm", "<CMD>Mason<CR>", desc = "Open Mason" },
         },
-        opts = {
-            ensure_installed = {
-                'lua-language-server',
-            },
-        },
-        config = function(_, opts)
-            require("mason").setup(opts)
-            local mr = require("mason-registry")
-            mr:on("package:install:success", function()
-                vim.defer_fn(function()
-                    -- trigger FileType event to possibly load this newly installed LSP server
-                    require("lazy.core.handler.event").trigger({
-                        event = "FileType",
-                        buf = vim.api.nvim_get_current_buf(),
-                    })
-                end, 100)
-            end)
-            local function ensure_installed()
-                for _, tool in ipairs(opts.ensure_installed) do
-                    local p = mr.get_package(tool)
-                    if not p:is_installed() then
-                        p:install()
-                    end
-                end
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(event)
+                    local opts = {buffer = event.buf}
+                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+                end,
+            })
+
+            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local default_setup = function(server)
+                require('lspconfig')[server].setup({
+                    capabilities = lsp_capabilities,
+                })
             end
-            if mr.refresh then
-                mr.refresh(ensure_installed)
-            else
-                ensure_installed()
-            end
+
+            local mason = require("mason")
+            local mason_lspconfig  = require("mason-lspconfig")
+
+            mason.setup()
+            mason_lspconfig.setup()
+            mason_lspconfig.setup_handlers {
+                default_setup
+            }
+
+            local cmp = require('cmp')
+            local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+            cmp.setup({
+                sources = {
+                    {name = 'nvim_lsp'},
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                }),
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+            })
         end,
     },
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
     "L3MON4D3/LuaSnip",
     {
         "numToStr/FTerm.nvim",
