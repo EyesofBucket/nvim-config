7a8b17f45bd7193b86ffe04db599df7144e0e8be
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2024-02-19 11:39:44 -0600

    Harpoon menu remap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -14,15 +14,62 @@ return {
         require("harpoon"):setup(opts)
     end,
     keys = {
-        { "<leader>a", function() require("harpoon"):list():append() end, desc = "Mark file", },
-        { "<leader>ph", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon menu", },
-
-        { "<C-h>", function() require("harpoon"):list():select(1) end, desc = "Jump to 1", },
-        { "<C-j>", function() require("harpoon"):list():select(2) end, desc = "Jump to 2", },
-        { "<C-k>", function() require("harpoon"):list():select(3) end, desc = "Jump to 3", },
-        { "<C-l>", function() require("harpoon"):list():select(4) end, desc = "Jump to 4", },
-
-        { "<C-S-P>", function() require("harpoon"):list():prev() end, desc = "Jump to previous", },
-        { "<C-S-N>", function() require("harpoon"):list():next() end, desc = "Jump to next", },
+        {
+            "<leader>a",
+            function()
+                require("harpoon"):list():append()
+            end,
+            desc = "Harpoon: Mark file",
+        },
+        {
+            "<leader>h",
+            function()
+                local harpoon = require("harpoon")
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end,
+            desc = "Harpoon: Open menu",
+        },
+        {
+            "<C-h>",
+            function()
+                require("harpoon"):list():select(1)
+            end,
+            desc = "Harpoon: Jump to 1",
+        },
+        {
+            "<C-j>",
+            function()
+                require("harpoon"):list():select(2)
+            end,
+            desc = "Harpoon: Jump to 2",
+        },
+        {
+            "<C-k>",
+            function()
+                require("harpoon"):list():select(3)
+            end,
+            desc = "Harpoon: Jump to 3",
+        },
+        {
+            "<C-l>",
+            function()
+                require("harpoon"):list():select(4)
+            end,
+            desc = "Harpoon: Jump to 4",
+        },
+        {
+            "<C-S-P>",
+            function()
+                require("harpoon"):list():prev()
+            end,
+            desc = "Harpoon: Jump to previous",
+        },
+        {
+            "<C-S-N>",
+            function()
+                require("harpoon"):list():next()
+            end,
+            desc = "Harpoon: Jump to next",
+        },
     },
 }
