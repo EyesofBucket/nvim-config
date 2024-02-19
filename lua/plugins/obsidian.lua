35e23f4b070a05121cdbf87ce9a6bf17dc4c961c
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2024-02-19 11:42:54 -0600

    Temporary workspace error fix

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,12 +1,20 @@
 return {
     "epwalsh/obsidian.nvim",
     version = "*",
+    lazy = true,
+    ft = "markdown",
     dependencies = {
         "hrsh7th/nvim-cmp",
         "nvim-lua/plenary.nvim",
         "ixru/nvim-markdown",
     },
     opts = {
-        disable_frontmatter = true
+        disable_frontmatter = true,
+        workspaces = {
+            {
+                name = "EyesofBucket",
+                path = "~/Obsidian/EyesofBucket",
+            },
+        },
     },
 }
