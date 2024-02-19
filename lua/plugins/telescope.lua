c260e8571f9efafa0df17a1b62e772c0523a0fa1
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2024-02-19 11:38:27 -0600

    Telescope help vsplit

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
+        'nvim-lua/plenary.nvim',
         {
             'nvim-telescope/telescope-fzf-native.nvim',
             build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
@@ -8,9 +9,48 @@ return {
     },
     version = "0.1.3",
     keys = {
-        { "<leader>pf", function() require('telescope.builtin').find_files() end, desc = "Telescope: Files" },
-        { "<leader>ps", function() require('telescope.builtin').live_grep() end, desc = "Telescope: Grep" },
-        { "<leader>pg", function() require('telescope.builtin').git_files() end, desc = "Telescope: Git" },
-        { "<leader>p?", function() require('telescope.builtin').help_tags() end, desc = "Telescope: Help" },
+        {
+            "<leader>pf",
+            function()
+                require('telescope.builtin').find_files()
+            end,
+            desc = "Telescope: Files"
+        },
+        {
+            "<leader>ps",
+            function()
+                require('telescope.builtin').live_grep()
+            end,
+            desc = "Telescope: Grep"
+        },
+        {
+            "<leader>pg",
+            function()
+                require('telescope.builtin').git_files()
+            end,
+            desc = "Telescope: Git"
+        },
+        {
+            "<leader>ph",
+            function()
+                local open_in_right_split = function(prompt_bufnr)
+                    local selection = require('telescope.actions.state').get_selected_entry()
+                    require('telescope.actions').close(prompt_bufnr)
+                    vim.cmd.help(selection.value)
+                    vim.cmd.wincmd('L')
+                end
+
+                require('telescope.builtin').help_tags({
+                    attach_mappings = function(_, map)
+                        map('i', '<CR>', open_in_right_split)
+                        map('n', '<CR>', open_in_right_split)
+                        return true
+                    end
+                })
+            end,
+            noremap = true,
+            silent = true,
+            desc = "Telescope: Help"
+        },
     },
 }
