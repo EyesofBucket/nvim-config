3bbd9474646b76da981571c03105dcdec6d740a4
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2024-03-27 13:04:01 -0500

    Added spelling, ansible-vim

--- lua/bvkt/lang.lua
+++ lua/bvkt/lang.lua
@@ -2,3 +2,8 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
   pattern = {"*.nix", "*.yaml", "*.yml", "*.json"},
   callback = function() vim.opt.shiftwidth = 2 end,
 })
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = {"*.md"},
+  callback = function() vim.cmd("setlocal spell") end,
+})
